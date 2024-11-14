4df286b6c79cf6317109a048c1e91bbb7c833dd8
tetie12345 <floristabak@gmail.com>
2024-11-14 11:45:10 +0100

    fixed utf-8 error and added debug mode

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-import os, time
+import os, time, sys
+
+mode = "normal"
+if len(sys.argv) > 1:
+    if sys.argv[1] == "debug":
+        mode = "debug"
 
 class ChatClient:
     def __init__(self):
+
+        self.line = 0
+        self.recievedMsgs = []
+
         server_input = getPromptedInput(1, 0, "Enter server address: ")
         if ':' in server_input:
             host, port = server_input.split(':')
@@ -48,8 +57,6 @@ class ChatClient:
         self.password = getPasswordPromptedInput(1, 0, "Enter encryption password:")
         self.key = self.derive_key(self.password)
 
-        self.line = 0
-        self.recievedMsgs = []
 
     def derive_key(self, password):
         """Derives a 32-byte key from the password."""
@@ -88,8 +95,9 @@ class ChatClient:
                     break
 
                 if data.startswith(b"Server:"):
-                    message = data.decode('utf-8').replace("Server:", "")
-                    displayMsg(message)
+                    message = data.decode('utf-8').replace("Server: ", "")
+                    msg = f"{time.strftime('%H:%M:%S', time.gmtime(time.time()))} SERVER: {message}"
+                    displayMsg(msg)
                 # Separate username and message
 
                 else:
@@ -101,8 +109,9 @@ class ChatClient:
                         msg = f"{time.strftime('%H:%M:%S', time.gmtime(time.time()))} {username}: {message}"
                         displayMsg(msg)
                     except Exception as e:
-                       print(e)
-
+                        if mode == "debug":
+                            clearLine(screen, 1)
+                            displayText(1, 0, f"ERROR: {e}")
             except Exception as e:
                 print(f"Connection error: {e}")
                 break
@@ -229,10 +238,10 @@ def displayText(screenPositionY, screenPositionX, text):
 def displayMsg(msg):
     app.recievedMsgs.append(msg)
     if 5+app.line == screen.getmaxyx()[0]-3:
-        app.recievedMsgs.pop(0)
         for i in range(len(app.recievedMsgs)):
             clearLine(screen, screen.getmaxyx()[0]-3-i)
             displayText(screen.getmaxyx()[0]-3-i, 0, app.recievedMsgs[len(app.recievedMsgs)-1-i])
+        app.recievedMsgs.pop(0)
     else:
         clearLine(screen, 5+app.line)
         displayText(5+app.line, 0, msg)
